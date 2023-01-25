6b704f9a8dd09b156daa1b924c78feab49aa9cad
Bektur <bekturhanazarov98@gmail.com>
2023-01-25 18:30:35 +0600

    asd

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> name=new LinkedList<>();
+        Random rn =new Random();
+        for (int i = 0; i < 20; i++) {
+            int a= rn.nextInt(0,2);
+            name.add(a);
+        }
+        System.out.println(name1(name));
+    }
+    public static LinkedList<Integer> name1(LinkedList<Integer> at) {
+    LinkedList<Integer> at1=new LinkedList<>();
+        for (int a:at) {
+            at1.add(a);
+        }
+        Collections.sort(at1);
+       return at1;
+    }
+
+}
